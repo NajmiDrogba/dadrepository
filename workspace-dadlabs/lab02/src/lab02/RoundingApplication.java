d6afb002eb6f462b8cd7480a0c47dcc6b3118c8f
Ahmad Najmi <B032110040@student.utem.edu.my>
2021-10-29 11:24:47 +0800

    Lab02
    Rounding Application

---
+++ workspace-dadlabs/lab02/src/lab02/RoundingApplication.java
@@ -0,0 +1,10 @@
+package lab02;
+
+public class RoundingApplication
+{
+	public static void main(String[] args)
+    {
+	   System.out.println("Rounding number to the nearest decimal points");
+    }
+
+}
