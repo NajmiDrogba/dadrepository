1c07abf237b5910753dbbc59145c4a69f6936687
Ahmad Najmi <B032110040@student.utem.edu.my>
2022-02-06 15:51:01 +0800

    lab02 Application
    class
    -Rounding Application
    -Rounding Manager
    image
    -output

--- workspace-dadlabs/lab02/src/lab02/RoundingApplication.java
+++ workspace-dadlabs/lab02/src/lab02/RoundingApplication.java
@@ -1,10 +1,29 @@
 package lab02;
 
+import lab02.rounding.RoundingManager;
+
 public class RoundingApplication 
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Rounding number to nearest decimal points");
+		//System.out.println("Rounding number to nearest decimal points");
+		
+		System.out.println("Main entry point of RoundingApplication");
+		
+		int divident = 200;
+		int divisor = 357;
+		RoundingManager roundManager = new RoundingManager();
+		double value = roundManager.calculatePercentage(divident, divisor);
+		
+		System.out.println("Progress from "+divisor+"/"+ divident + "." + value + "%");
+		
+		int decimalPoints = 2;
+		
+		System.out.println("Round to " + decimalPoints + " decimal Points " + roundManager.roundValue(value, decimalPoints)+"%");
+		
+		decimalPoints = 0;
+		System.out.println("Round to " + decimalPoints + " decimal Points " + roundManager.roundValue(value, decimalPoints)+"%");
+		
 	}
 
 }
---
+++ workspace-dadlabs/lab02/src/lab02/rounding/RoundingManager.java
@@ -0,0 +1,32 @@
+package lab02.rounding;
+
+import java.text.DecimalFormat;
+
+public class RoundingManager 
+{
+	public double calculatePercentage(int dividend, int divisor)
+	{
+		double percentage = ((double)dividend/(double)divisor)*100;
+		
+		return percentage;
+	}
+
+	public String roundValue(double value, int decimalPoints)
+	{
+		String decimalPlaces = ".";
+		for (int counter = 0; counter < decimalPoints; counter++) 
+		{
+			decimalPlaces +="0";
+		}
+		
+		if(decimalPoints == 0)
+		{
+			decimalPlaces = "";
+		}
+		
+		DecimalFormat formatter = new DecimalFormat("###"+decimalPlaces);
+		String result = formatter.format(value);
+		
+		return result;
+	}
+}
